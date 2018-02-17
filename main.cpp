9af67a97dd2e330ee63c797b8b55a66e645efe50
Egor <egor-nesterenko@ukr.net>
2018-02-18 00:46:34 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,39 @@
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+
+
+class Kata
+{
+public:
+    int countInversions(std::vector<int> array)
+    {
+        int counter = 0;
+        bool swapp = true;
+        while(swapp)
+        {swapp = false;
+            for (int i = 0; i < array.size()-1; i++)
+            {
+                if (array[i]>array[i+1] )
+                {counter++;
+                    array[i] += array[i+1];
+                    array[i+1] = array[i] - array[i+1];
+                    array[i] = array[i+1];
+                    swapp = true;
+                    
+                };
+                
+            } }
+        return counter;
+    }
+};
+
+int main() {
+    Kata one;
+    one.countInversions({2,1,4,3,5});
+    
+}
