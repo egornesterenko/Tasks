597ccab558b9d8f8986481f2b675c8a4bf72e508
Egor <egor-nesterenko@ukr.net>
2018-02-22 10:49:43 +0200

    Update calc_arr.cpp

--- calc_arr.cpp
+++ calc_arr.cpp
@@ -1,39 +1,26 @@
-
-#include <iostream>
-#include <string>
-#include <vector>
-
-using namespace std;
-
-
-
 class Kata
 {
 public:
-    int countInversions(vector<int> array)
+    int countInversions(std::vector<int> array)
     {
         int counter = 0;
         bool swapp = true;
         while(swapp)
-        {swapp = false;
-            for (int i = 0; i < array.size()-1; i++)
+        {
+            swapp = false;
+            for (int i = 0; i+1 < array.size(); i++)
             {
                 if (array[i]>array[i+1] )
-                {counter++;
-                    array[i] += array[i+1];
-                    array[i+1] = array[i] - array[i+1];
+                {   counter++;
+                    int tem = array[i];
                     array[i] = array[i+1];
+                    array[i+1] = tem;
                     swapp = true;
-                    
-                };
-                
-            } }
+                }
+            }
+            if (array.size() == 0) {return counter;}
+            
+        };
         return counter;
     }
 };
-
-int main() {
-    Kata one;
-    one.countInversions({2,1,4,3,5});
-    
-}
